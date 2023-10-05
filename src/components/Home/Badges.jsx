33879f981d64bbd3ed1aa58d3c4c41832a35d061
saraoriola <saraoriola@outlook.com>
2023-10-05 17:06:11 +0200

    Badges terminadas

--- src/components/Home/Badges.jsx
+++ src/components/Home/Badges.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Badge, Box } from '@chakra-ui/react';
 
 const Badges = () => {
-  return (
-    <div>Badges</div>
-  )
-}
+  const badgeStyles = {
+    background: '#262626',
+    color: '#99F5F2',
+    fontSize: '0.8rem',
+    fontWeight: 'bold',
+    borderRadius: '20px',
+    paddingLeft: '10px',
+    paddingRight: '10px',
+    marginLeft: '6px',
+  };
 
-export default Badges
+  return (
+    <>
+      <Badge style={badgeStyles}>Conciertos</Badge>
+      <Badge style={badgeStyles}>Festivales de comida</Badge>
+      <Badge style={badgeStyles}>Conferencias</Badge>
+      <Badge style={badgeStyles}>Triatlón</Badge>
+      <Badge style={badgeStyles}>Motos</Badge>
+      <Badge style={badgeStyles}>Parques temáticos</Badge>
+      <Badge style={badgeStyles}>Exposiciones</Badge>
+      <Badge style={badgeStyles}>Surf</Badge>
+    </>
+  );
+};
+
+export default Badges;
