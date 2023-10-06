c34a780adf2fc1c79b40f1eefc94bcaf8c35a5a8
saraoriola <saraoriola@outlook.com>
2023-10-06 09:51:56 +0200

    Buttons ok

--- src/components/Bookings/MyBookings/Bookings.jsx
+++ src/components/Bookings/MyBookings/Bookings.jsx
@@ -24,16 +24,19 @@ const Bookings = () => {
   }, [dispatch]);
 
   return (
-    <div>
+    <div style={{ background: '#262626', minHeight: '100vh', color: 'white' }}>
       <Header />
-      <h1>Mis Reservas</h1>
-      <Flex flexWrap="wrap">
+      <Text fontSize="3xl" fontWeight="black" ml={10} mt={5}>
+        Mis reservas
+      </Text>
+      <Flex flexWrap="wrap" ml={8} mt={5}>
         {userBookings.map((booking) => (
           <Card key={booking.id} maxW="sm" m="4">
             <Image
               src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
               alt="Imagen del evento"
               borderRadius="lg"
+              m={5}
             />
             <CardBody>
               <Stack mt="6" spacing="3">
@@ -47,13 +50,44 @@ const Bookings = () => {
               </Stack>
             </CardBody>
             <Divider />
-            <ButtonGroup spacing="2" m="4">
-              <Button variant="solid" colorScheme="blue">
+            <ButtonGroup spacing="2" m="4" mx="auto">
+              <Button
+                variant="solid"
+                colorScheme="blue"
+                w="150px" 
+                bg="transparent"
+                borderRadius="full"
+                borderColor="#99F5F2"
+                borderWidth="2px"
+                color="black"
+                _hover={{
+                  borderColor: '#00FFFF',
+                  boxShadow: '0 0 8px 0 #00FFFF',
+                  transition: 'border-color 0.3s ease-out, box-shadow 0.3s ease-out',
+                }}
+              >
                 Editar
               </Button>
-              <Button variant="ghost" colorScheme="red">
-                Cancelar
-              </Button>
+              <Button
+  variant="ghost"
+  colorScheme="red"
+  ml={4}
+  w="150px"
+  bg="transparent"
+  borderRadius="full"
+  borderColor='red'
+  borderWidth="2px"
+  color="black"
+  _hover={{
+    borderColor: 'red',
+    boxShadow: '0 0 8px 0 red', // Agregamos una sombra roja en el hover
+    transition: 'border-color 0.3s ease-out, box-shadow 0.3s ease-out',
+  }}
+>
+  Cancelar
+</Button>
+
+
             </ButtonGroup>
           </Card>
         ))}
