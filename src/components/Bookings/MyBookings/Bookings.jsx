533db8ea024a8e6d9015374a3713067188613292
saraoriola <saraoriola@outlook.com>
2023-10-06 09:41:41 +0200

    Card montada

--- src/components/Bookings/MyBookings/Bookings.jsx
+++ src/components/Bookings/MyBookings/Bookings.jsx
@@ -2,6 +2,18 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMyBookings } from '../../../features/bookings/bookingsSlice';
 import Header from '../../Layout/Header/Header';
+import {
+  Flex,
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Button,
+  ButtonGroup,
+  Divider,
+} from '@chakra-ui/react';
 
 const Bookings = () => {
   const dispatch = useDispatch();
@@ -13,21 +25,39 @@ const Bookings = () => {
 
   return (
     <div>
-      <Header/>
+      <Header />
       <h1>Mis Reservas</h1>
-      <ul>
+      <Flex flexWrap="wrap">
         {userBookings.map((booking) => (
-          <li key={booking.id}>
-            <h2>Reserva</h2>
-            <p>Estado: {booking.status}</p>
-            <p>Descripción: {booking.description}</p>
-            <h2>Evento Asociado</h2>
-            <p>Título del evento: {booking.Event.title}</p>
-            <p>Fecha del evento: {booking.Event.date}</p>
-            <p>Ubicación del evento: {booking.Event.location}</p>
-          </li>
+          <Card key={booking.id} maxW="sm" m="4">
+            <Image
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Imagen del evento"
+              borderRadius="lg"
+            />
+            <CardBody>
+              <Stack mt="6" spacing="3">
+                <Heading size="md">Reserva</Heading>
+                <Text>Estado: {booking.status}</Text>
+                <Text>Descripción: {booking.description}</Text>
+                <Heading size="md">Evento Asociado</Heading>
+                <Text>Título del evento: {booking.Event.title}</Text>
+                <Text>Fecha del evento: {booking.Event.date}</Text>
+                <Text>Ubicación del evento: {booking.Event.location}</Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <ButtonGroup spacing="2" m="4">
+              <Button variant="solid" colorScheme="blue">
+                Editar
+              </Button>
+              <Button variant="ghost" colorScheme="red">
+                Cancelar
+              </Button>
+            </ButtonGroup>
+          </Card>
         ))}
-      </ul>
+      </Flex>
     </div>
   );
 };
