1ac05bc1b2f6f99f3dae69e6cd42bce792c4978c
saraoriola <saraoriola@outlook.com>
2023-10-06 11:03:52 +0200

    HOME OK

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,10 +16,10 @@ const Home = () => {
       <Header />
       <Box fontSize="xl">
         {user && <Text fontSize="3xl" fontWeight="black" ml={10} mt={5}>¡Hola, {user.name}!</Text>}
-        <Grid templateColumns="70% 30%" ml={10} mt={5} height="100vh">
+        <Grid templateColumns="70% 30%" ml={10} mt={5}>
           <GridItem colSpan={1}>
             <Box height="100%">
-              <Card width="100%" height="30%" backgroundImage={`linear-gradient(transparent, rgba(0,0,0,0.8)), url(${evento})`} backgroundSize="cover" backgroundPosition="center" backgroundRepeat="no-repeat" borderRadius={"20"} direction="column" overflow='hidden' variant='outline'>
+              <Card width="1150px" height="95%" backgroundImage={`linear-gradient(transparent, rgba(0,0,0,0.8)), url(${evento})`} backgroundSize="cover" backgroundPosition="center" backgroundRepeat="no-repeat" borderRadius={"20"} direction="column" overflow='hidden' variant='outline'>
                 <CardHeader>
                   <Flex spacing='2' alignItems='center'>
                     <Avatar size='md' name='P C' />
@@ -36,9 +36,6 @@ const Home = () => {
                   </Box>
                 </CardFooter>
               </Card>
-              <Text fontSize="xl" fontWeight="black" mt={5}>Galerías más populares</Text>
-              <Box mt={2}><Badges /></Box>
-              <Box mt={5}><Events/></Box>
             </Box>
           </GridItem>
           <GridItem colSpan={1} ml={5} mr={5}>
@@ -81,6 +78,13 @@ const Home = () => {
             </Box>
           </GridItem>
         </Grid>
+      </Box>
+      <Box ml={6}>
+
+      <Text fontSize="xl" fontWeight="black" >Galerías más populares</Text>
+              <Box mt={2}><Badges /></Box>
+              <Box mt={5}><Events/></Box>
+
       </Box>
     </>
   );
